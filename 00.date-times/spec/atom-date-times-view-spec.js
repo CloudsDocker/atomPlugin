7716c270e8b6bb78c1ec3fcbf01a6c5b2d8160f4
todzhang's cloudsdocker <phray.zhang@gmail.com>
2017-01-23 14:55:05 +0800

    Create atom-date-times-view-spec.js

---
+++ 00.date-times/spec/atom-date-times-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDateTimesView from '../lib/atom-date-times-view';
+
+describe('AtomDateTimesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
